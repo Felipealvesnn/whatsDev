a38a0969393996d89d031bafe16d451109e58203
felipe alves nunes <felipe.alvesnn@gmail.com>
2024-05-22 22:27:38 -0300

    init

---
+++ src/pages/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page class="">
+    <div>
+      <h5>Acesse o  chat</h5>
+      <q-input rounded outlined v-model="email" label="
+      informe o seu E-mail" bg-color="White"
+      class="q-mb-md"
+      >
+
+
+      </q-input>
+    </div>
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      email: ''
+    }
+  }
+}
+</script>
