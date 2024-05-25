6c0987843c38b3fadade7a4abed326fe982d7b00
felipe alves nunes <felipe.alvesnn@gmail.com>
2024-05-24 23:02:33 -0300

    att

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -2,7 +2,7 @@ module.exports = {
     dialect: 'postgres', // Certifique-se de que o dialect está correto
     host: 'localhost',
     username: 'whatzasDev',
-    password: 'whatzasDev',
+    password: 'password',
     database: 'dev2dev',
     port: 5432, // Porta padrão do PostgreSQL
     define: {
